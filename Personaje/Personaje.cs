c121e2ce523ceaf37ad8cbce832590967255b3aa
FERNANDO O GUAZO <fguazo@gmail.com>
2023-05-03 23:05:27 -0600

    first commit

---
+++ Personaje/Personaje.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulacion_Actividad_1_U1_FEGA.Personaje
+{
+    public class Personaje
+    {
+        public string Nombre { get; private set; }
+        public string NombreReal { get; private set; }
+        public string PrimeraAparicion { get; private set; }
+        public string FechaPrimeraAparicion { get; set; }
+        public string Poderes { get; set; }
+
+        public string Creador { get; set; }
+        public string Artista { get; set; }
+
+        public void CrearPersonaje (string _nombre, string _nombreReal, string _primeraAparacion)
+        {
+            this.Nombre = _nombre;
+            this.NombreReal = _nombreReal;
+            this.PrimeraAparicion = _primeraAparacion;
+        }
+
+        public void MostrarPersonaje(Personaje _personaje)
+        {
+            Console.WriteLine("Nombre: " + _personaje.Nombre);
+            Console.WriteLine("Nombre Real: " + _personaje.NombreReal);
+            Console.WriteLine("primera Aparacion: " + _personaje.PrimeraAparicion);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Se crea el primer personaje utilizando el metodo de la clase personaje.
+
+using Encapsulacion_Actividad_1_U1_FEGA.Personaje;
+
+Personaje wonderwoman=new Personaje();
+
+wonderwoman.CrearPersonaje("Wonder Woman", "Diana", "All Star Comics VI #8, dic 1941, Sept,2011");
+
+/*Console.WriteLine("Nombre: " + WonderWoman.Nombre);
+Console.WriteLine("Nombre Real: " + WonderWoman.NombreReal);
+Console.WriteLine("primera Aparacion: " +WonderWoman.PrimeraAparicion);*/
+
+wonderwoman.MostrarPersonaje(wonderwoman);
+
+
+
